296683429a5035909d5fe31cd3e907de8bed5eac
nonsodaniel <nonsodaniel07@gmail.com>
2020-01-01 16:08:08 +0100

    Used useState to fetch and display data

--- src/components/layouts/SongList.js
+++ src/components/layouts/SongList.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const SongList = () => {
+    const [songs, setSongs] = useState([
+        { title: "joro", id: Math.random() },
+        { title: "Africa Giant", id: Math.random() },
+        { title: "Risky", id: Math.random() },
+        { title: "Vibration", id: Math.random() },
+
+    ])
     return (
         <div className="song-list">
             <ul>
-                <li>Joro</li>
-                <li>JAfrica Giant</li>
+                {
+                    songs && songs.map(song => {
+                        return (<li key={song.id}>{song.title}</li>)
+                    })
+                }
             </ul>
         </div>
     )
